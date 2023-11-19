7dbdac1ba38cdf73db0c5eea38015085593589d2
PranshuBasak <pranshubasak@gmail.com>
2023-11-19 23:50:59 +0530

    First Commit

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,14 @@
+import Prompt from "@models/prompt";
+import { connectionToDB } from "@utils/database";
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectionToDB()
+
+        const prompts = await Prompt.find({ creator: params.id }).populate("creator")
+
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch prompts created by user", { status: 500 })
+    }
+} 
---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Feed from '@components/Feed'
+
+const Home = () => (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+      Create & Inspire
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center"> Spark Creativity, Share Prompts</span>
+      </h1>
+      <p className="desc text-center">PromptNest is a dynamic full-stack application designed to serve as a hub for creative minds to share, explore, and engage with prompts across various genres and mediums.</p>
+
+      <Feed />
+    </section>
+  );
+
+export default Home
