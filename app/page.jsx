a0573ce735fc75db623ca7ec18437ace6b243863
PranshuBasak <pranshubasak@gmail.com>
2023-11-20 00:02:15 +0530

    modified

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,7 @@ import Feed from '@components/Feed'
 const Home = () => (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-<<<<<<< HEAD
       Promptnest
-=======
-      Create & Inspire
->>>>>>> 7dbdac1ba38cdf73db0c5eea38015085593589d2
       <br className="max-md:hidden" />
       <span className="orange_gradient text-center"> Spark Creativity, Share Prompts</span>
       </h1>
